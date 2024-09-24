a1cb63428594e01f108311699380c1bac14f710b
mariotbg11 <christofell.m@gmail.com>
2024-09-24 13:00:04 +0700

    feat: fetching api and display the data currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
 import Table from "./components/Table";
 
 function App() {
+  const [currencyRates, setCurrencyRates] = useState([]);
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_CURRENCY_API_KEY;
+    const url = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}`;
+
+    async function getCurrency() {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data.rates);
+      setCurrencyRates(data.rates);
+    }
+    getCurrency();
+  }, []);
+
+  // We Buy
+  const calcBuy = (rate) => (rate * 1.02).toFixed(4);
+
+  // We Sell
+  const calcSell = (rate) => (rate * 0.98).toFixed(4);
+
   return (
     <div className="h-screen bg-orange-500 flex items-center justify-center">
-      <Table />
+      <Table
+        currencyRates={currencyRates}
+        calcBuy={calcBuy}
+        calcSell={calcSell}
+      />
     </div>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,4 @@
-function Table() {
+function Table({ currencyRates, calcBuy, calcSell }) {
   return (
     <div className="overflow-x-auto table-lg">
       <table className="table">
@@ -17,44 +17,44 @@ function Table() {
           {/* row 1 */}
           <tr className="border-b border-[#e7e7e76d]">
             <th className="font-semibold">CAD</th>
-            <td>001.7434</td>
-            <td>001.7092</td>
-            <td>001.6750</td>
+            <td>{calcBuy(currencyRates.CAD)}</td>
+            <td>{parseFloat(currencyRates.CAD).toFixed(4)}</td>
+            <td>{calcSell(currencyRates.CAD)}</td>
           </tr>
           {/* row 2 */}
           <tr className="border-b border-[#e7e7e76d]">
             <th className="font-semibold">IDR</th>
-            <td>18522.3525</td>
-            <td>18159.1691</td>
-            <td>17795.9857</td>
+            <td>{calcBuy(currencyRates.IDR)}</td>
+            <td>{parseFloat(currencyRates.IDR).toFixed(4)}</td>
+            <td>{calcSell(currencyRates.IDR)}</td>
           </tr>
           {/* row 3 */}
           <tr className="border-b border-[#e7e7e76d]">
             <th className="font-semibold">JPY</th>
-            <td>143.4565</td>
-            <td>140.6437</td>
-            <td>137.8308</td>
+            <td>{calcBuy(currencyRates.JPY)}</td>
+            <td>{parseFloat(currencyRates.JPY).toFixed(4)}</td>
+            <td>{calcSell(currencyRates.JPY)}</td>
           </tr>
           {/* row 4 */}
           <tr className="border-b border-[#e7e7e76d]">
             <th className="font-semibold">CHF</th>
-            <td>001.3114</td>
-            <td>001.2857</td>
-            <td>001.2600</td>
+            <td>{calcBuy(currencyRates.CHF)}</td>
+            <td>{parseFloat(currencyRates.CHF).toFixed(4)}</td>
+            <td>{calcSell(currencyRates.CHF)}</td>
           </tr>
           {/* row 5 */}
           <tr className="border-b border-[#e7e7e76d]">
             <th className="font-semibold">EUR</th>
-            <td>001.1937</td>
-            <td>001.1703</td>
-            <td>001.1469</td>
+            <td>{calcBuy(currencyRates.EUR)}</td>
+            <td>{parseFloat(currencyRates.EUR).toFixed(4)}</td>
+            <td>{calcSell(currencyRates.EUR)}</td>
           </tr>
           {/* row 6 */}
           <tr className="border-b border-[#e7e7e76d]">
             <th className="font-semibold">USD</th>
-            <td>001.3141</td>
-            <td>001.2884</td>
-            <td>001.2626</td>
+            <td>{calcBuy(currencyRates.USD)}</td>
+            <td>{parseFloat(currencyRates.USD).toFixed(4)}</td>
+            <td>{calcSell(currencyRates.USD)}</td>
           </tr>
         </tbody>
       </table>
